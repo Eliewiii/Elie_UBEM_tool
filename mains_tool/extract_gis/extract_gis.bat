7a7b37acb6d262adde8cb610a7b4e3d8c1cdc2ed
Elie_home <elie.medioniwiii@gmail.com>
2023-02-26 14:21:50 +0200

    adjustments test_file

--- mains_tool/extract_gis/extract_gis.bat
+++ mains_tool/extract_gis/extract_gis.bat
@@ -2,7 +2,7 @@
 
 rem Set variables
 set path_venv_script=%LOCALAPPDATA%\Building_urban_analysis\tool_venv\Scripts
-set path_python_scripts=%LOCALAPPDATA%\Building_urban_analysis\Scripts"
+set path_python_scripts=%LOCALAPPDATA%\Building_urban_analysis\Scripts
 
 echo Activate the virtual environment
 call "%path_venv_script%\activate.bat"
